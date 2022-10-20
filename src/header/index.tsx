587babe744713a9e7591b6b00748b0e363f0e9fa
50295184+habbababbai@users.noreply.github.com <50295184+habbababbai@users.noreply.github.com>
2022-10-20 16:46:39 +0200

    initial commit
    header, scss

---
+++ src/header/index.tsx
@@ -0,0 +1,16 @@
+import "./header.scss";
+
+function Header() {
+    return (
+        <div id="bg">
+            <div>
+                <span id="t1">Hi</span>
+                <span id="t2">, i'm </span>
+                <span id="t3">bai</span>
+                <br></br>
+                <span id="t4">And this is my website</span>
+            </div>
+        </div>
+    );
+}
+export default Header;
