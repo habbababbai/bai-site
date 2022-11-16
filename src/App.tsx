79ce51c5b1e8698b27a04ac6bd4af8952f79f7b9
habbababbai <qba.7145@gmail.com>
2022-11-16 13:07:05 +0100

    vuln fix
    Prototype pollution in webpack loader-utils #3

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <div className="App">
             <Header></Header>
             <About></About>
-            <FetchDemo></FetchDemo>
+
             <Contact></Contact>
             <Footer></Footer>
         </div>
